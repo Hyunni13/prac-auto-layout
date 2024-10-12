854db0719dbebc1c0e7470f2061c4fe285d24df4
Sanghyun <dev.sanghyun@gmail.com>
2024-10-12 13:49:12 +0900

    [CHORE] 프로젝트 생성 및 디렉토리 구조화, 개발 초기 환경 구성

---
+++ prac-auto-layout/Core/Utils/Logger.swift
@@ -0,0 +1,21 @@
+//
+//  Logger.swift
+//  prac-auto-layout
+//
+//  Created by James on 10/11/24.
+//
+
+final class Logger {
+    
+    private static let defaultMessage = "Call here."
+    
+    /// 커스텀 로깅 함수
+    ///
+    /// 로그 구성
+    /// - 로깅 위치(로그를 찍는 함수)
+    /// - 로그 메시지
+    static func log(_ message: String = defaultMessage, function: String = #function) {
+        print(function, message)
+    }
+    
+}
