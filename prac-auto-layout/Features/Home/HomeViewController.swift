8ad24e88a4f8b5620bca7f8ba01e751e7c50ade1
Sanghyun <dev.sanghyun@gmail.com>
2024-10-18 10:40:29 +0900

    [ADD] HomeViewController(UIKit)에 SplashView(SwiftUI) 포함

--- prac-auto-layout/Features/Home/HomeViewController.swift
+++ prac-auto-layout/Features/Home/HomeViewController.swift
@@ -5,6 +5,7 @@
 //  Created by James on 10/11/24.
 //
 
+import SwiftUI
 import UIKit
 
 final class HomeViewController: UIViewController {
@@ -16,6 +17,31 @@ final class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        renderUI()
+        bindViewModel()
+    }
+    
+    /// UI 그리기
+    private func renderUI() {
+        // HomeViewController 하위에 SplashView 넣어주기
+        let splash = SplashView()
+        let splashHost = UIHostingController(rootView: splash)
+        addChild(splashHost)
+        view.addSubview(splashHost.view)
+        didMove(toParent: self)
+        
+        // SplashView 위치 잡기
+        splashHost.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            splashHost.view.topAnchor.constraint(equalTo: view.topAnchor),
+            splashHost.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            splashHost.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            splashHost.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
+    }
+    
+    /// ViewModel 연동
+    private func bindViewModel() {
         getViewModel().sideEffect = { [weak self] sideEffect in
             guard let self else { return }
             
