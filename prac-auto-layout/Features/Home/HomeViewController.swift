98102bdcf7a8e1be74bd9e02e02cb3b75d360f98
Sanghyun <dev.sanghyun@gmail.com>
2024-10-25 19:58:48 +0900

    [UPDATE] HomeViewController Storyboard 의존성 제거
    - AccountView 커스텀뷰 구현
    - HomeViewController에 StackView 구현

--- prac-auto-layout/Features/Home/HomeViewController.swift
+++ prac-auto-layout/Features/Home/HomeViewController.swift
@@ -9,8 +9,38 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    // StackView
+    private lazy var stackView: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .vertical
+        stack.distribution = .fillEqually
+        stack.alignment = .fill
+        stack.spacing = 10
+        return stack
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor.systemYellow
+        
+        (0..<10).forEach { _ in
+            stackView.addArrangedSubview(AccountView())
+        }
+        
+        view.addSubview(stackView)
+        pinStackView()
+    }
+    
+    private func pinStackView() {
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            stackView.leadingAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.leadingAnchor,
+                constant: 20),
+            stackView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+        ])
     }
     
 }
